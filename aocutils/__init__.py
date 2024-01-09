e58fa63ffb5167b2ff744baf0aa385eac0be4fa9
Ahmed Elashry <ahmedelashry70@gmail.com>
2024-01-09 23:11:05 +0100

    delete extra init file

--- aocutils/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-from .aocutils.grid import Grid
-from .aocutils.parse import Parser
-from .aocutils.interval import IntInterval, Subset
-from .aocutils.utils import timeit
-from .aocutils.map import Mapper
